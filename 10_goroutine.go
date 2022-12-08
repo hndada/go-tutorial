2c17122c8e93cf205986192b2eb4889b3c2f11c1
Muang <hndada@dgist.ac.kr>
2022-12-08 23:41:02 +0900

    minimize

--- 10_goroutine.go
+++ 10_goroutine.go
@@ -2,34 +2,12 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-func hard() {
-	for i := 0; i < 2*1e9; i++ {
-	}
-	fmt.Println("hard work done")
-}
-func easy() {
-	for i := 0; i < 10; i++ {
-	}
-	fmt.Println("easy work done")
-}
 func main() {
-	go hard()
-	easy()
-	time.Sleep(2 * time.Second) // need to wait hard()
-	fmt.Println()
-
-	// wait groups: nicer than time.Sleep()
-	var wg sync.WaitGroup
-	wg.Add(1) // wg's counter: 0 -> 1
-	go func() {
-		defer wg.Done() // wg's counter: 1 -> 0
-		hard()
-	}()
-
-	easy()
-	wg.Wait() // wait until wg's counter is 0
+	for i := 0; i < 5; i++ {
+		go fmt.Println(i) // call function in parallel
+	}
+	time.Sleep(time.Second) // better: use WaitGroup
 }
